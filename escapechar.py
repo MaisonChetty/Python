fb809650b9ea41694417cc328680f651fdc8f062
MaisonChetty <79193288+MaisonChetty@users.noreply.github.com>
2021-02-17 11:01:12 +0200

    Create escapechar.py

---
+++ escapechar.py
@@ -0,0 +1,15 @@
+splitString ="this string has been\nsplit over\nseveral\nlines"
+print(splitString)
+
+tabbedString="1\t2\t3\t4\t5"
+print(tabbedString)
+
+print('The pet shop owner said "NO ,no, \'e\'s uh,...he\'s resting".')
+
+print('''The pet shop owner said "NO ,no, 'e's uh,...he's resting".''')
+
+AnothersplitString='''this string has been
+split over
+several
+lines'''
+print(AnothersplitString)
